65fcdf748dd11f320e9e70a6a1c76679d1ec0e62
suzieisa <isasuzm@gmail.com>
2024-01-03 20:51:29 -0600

    first

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
